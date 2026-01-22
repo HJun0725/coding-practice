d167dcdb250df49c32e64d1cfdadfe8531791c95
HJun0725 <80038863+HJun0725@users.noreply.github.com>
2026-01-22 14:30:17 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -1,2 +1,8 @@
 def solution(n, numlist):
     return [i for i in numlist if i%n == 0]
+
+'''
+새롭게 알게 된 것 - return list(filter(lambda v: v%n==0, numlist))
+filter 함수는 인자 값으로 (True/False 반환 함수, 반복 가능 객체)를 받아
+객체의 요소에서 True 값만 반환한다.
+'''
