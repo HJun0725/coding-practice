792186d5aa01c598127f0e1c63e39457257b3e4c
HJun0725 <80038863+HJun0725@users.noreply.github.com>
2026-01-22 14:42:22 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,2 +1,8 @@
 def solution(money):
-    return [money//5500, money - money//5500 * 5500]
+    return [money//5500, money%5500]
+
+'''
+새롭게 알게 된 것 - return divmod(money, 5500)
+divmod 함수는 (나누어지는 값, 나누는 값)를 인자값으로 받아
+몫과 나머지를 튜플로 반환함.
+'''
