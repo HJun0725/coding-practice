f4cbe9c3ba566e14a7cc164160dd0ef1621d8baf
HJun0725 <80038863+HJun0725@users.noreply.github.com>
2026-01-22 14:35:52 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -1,2 +1,8 @@
 def solution(strlist):
     return [len(i) for i in strlist]
+
+'''
+새롭게 알게 된 것 - return list(map(len, strlist))
+map 함수는 (변환 함수, 반복 가능 객체)를 인자값으로 받아
+객체의 각 요소를 함수를 통해 변환된 값을 반환한다.
+'''
