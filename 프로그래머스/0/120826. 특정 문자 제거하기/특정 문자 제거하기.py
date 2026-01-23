f0368ac17abea70ef921571fd0325033f05068ff
HJun0725 <80038863+HJun0725@users.noreply.github.com>
2026-01-23 22:17:41 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,2 +1,8 @@
 def solution(my_string, letter):
     return ''.join([i for i in my_string if i != letter])
+
+'''
+새롭게 알게 된 것 - return my_string.replace(letter, '')
+replace 함수는 인자값으로 (수정할 문자, 수정 내용)을 받아 
+수정된 문자열을 리턴
+'''
