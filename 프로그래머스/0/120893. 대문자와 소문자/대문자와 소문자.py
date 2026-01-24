54432566f2a6fafbbb922aad059e3dfcd5c420bb
HJun0725 <80038863+HJun0725@users.noreply.github.com>
2026-01-24 15:23:33 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.14 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,2 +1,7 @@
 def solution(my_string):
     return ''.join([i.upper() if i.islower() else i.lower() for i in my_string ])
+
+'''
+새롭게 알게 된 것 - return my_string.swapcase()
+대문자는 소문자로, 소문자는 대문자로 변환된 문자열을 리턴
+'''
