37e3fa38e8d6c4b94153e475aad5c17c60e8b42e
HJun0725 <80038863+HJun0725@users.noreply.github.com>
2026-01-28 20:45:41 +0900

    [level 0] Title: 수 조작하기 1, Time: 5.57 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -3,3 +3,9 @@ def solution(n, control):
     for i in control:
         n += alph[i]
     return n
+
+'''
+새롭게 알게 된 것 - zip()
+두 개 이상의 반복 가능한(iterable) 객체를 인자로 받아, 
+각 객체의 동일한 인덱스에 위치한 요소들을 튜플로 묶어주는 내장 함수
+'''
