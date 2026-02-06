3ed21728c4be8f8dcc40e178c2fe9a12760d1b81
HJun0725 <80038863+HJun0725@users.noreply.github.com>
2026-02-06 23:36:46 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,2 +1,8 @@
 def solution(todo_list, finished):
     return [todo_list[i] for i in range(len(finished)) if finished[i] == False]
+
+'''
+새롭게 알게 된 것 - enumerate
+enumerate 함수는 반복 가능한 객체를 인자로 받아 
+그 객체의 인덱스와 값을 튜플 타입으로 반환함
+'''
