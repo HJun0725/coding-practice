697e04fcfd220b3defa5d549cdc3787d30d71516
HJun0725 <80038863+HJun0725@users.noreply.github.com>
2026-03-13 18:07:52 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.11 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,5 +1,4 @@
 def solution(l, r):
     max_num = "1000000"
-    zero_five = [5 * int(bin(i)[2:]) for i in range(1, 2**len(max_num)+1)]
-    answer = [i for i in zero_five if l <= i <= r]
+    answer = [5 * int(bin(i)[2:]) for i in range(1, 2**len(max_num)+1) if l <= 5 * int(bin(i)[2:]) <= r]
     return [-1] if answer == [] else answer
